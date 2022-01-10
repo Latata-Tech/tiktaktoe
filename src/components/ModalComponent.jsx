fe1e99b34b60ae1c32ba2649f20b2d319295ece1
rizkypakpahan <rizky.kurniawanpakpahan@gmail.com>
2022-01-10 12:04:28 +0700

    edit modal

--- src/components/ModalComponent.jsx
+++ src/components/ModalComponent.jsx
@@ -20,43 +20,39 @@ const ModalComponent = (props) => {
         ])
     }
     return (
+        
         <div className="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true" style={{display: props.display}}>
-            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div className="flex items-end justify-center pt-10 px-4 pb-20 text-center sm:block sm:p-0 sm:h-4/5">
 
                 <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
 
                 <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 
-                <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                    <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                        <div className="sm:flex sm:items-start">
-                            <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-
-                            </div>
-                            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-                                    Deactivate account
+                <div className="inline-block align-bottom bg-white/80 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:align-middle sm:w-8/12 sm:h-full">
+                    <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                        <div className="text-center">
+                            <div className="mt-3 text-center">
+                                <h3 className="text-9xl leading-6 font-extrabold text-gray-900 text-center sm:mt-96" id="modal-title">
+                                    Win!
                                 </h3>
-                                <div className="mt-2">
+                                <div className="mt-20">
                                     <p className="text-sm text-gray-500">
-                                        Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.
+                                        Lose
                                     </p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                        <button onClick={()=> resetBoard()} type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
-                            Deactivate
-                        </button>
-                        <button type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-                            Cancel
+                    <div className="px-4 py-3 sm:px-6 flex justify-center mx-auto ">
+                        <button onClick={()=> resetBoard()} type="button" className="inline-flex w-96 rounded-md border border-transparent shadow-sm px-6 py-5 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 justify-center">
+                            Restart The Game
                         </button>
                     </div>
                 </div>
             </div>
         </div>
     )
+    
 }
 
 export default ModalComponent
