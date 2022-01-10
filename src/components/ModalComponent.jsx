1ceab918c6e5cb9ecd53c6a89dc6a2b10049e16b
farhan <farhanafriza24@gmail.com>
2022-01-10 20:57:18 +0700

    fix: bug when draw, image display vertical

--- src/components/ModalComponent.jsx
+++ src/components/ModalComponent.jsx
@@ -49,11 +49,11 @@ const ModalComponent = (props) => {
                     <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                         <div className="text-center">
                             <div className="mt-3 text-center">
-                                <div id="container-winner" className="grid grid-cols-1 gap-1 place-items-center">
+                                <div id="container-winner" className={"grid grid-cols-"+props.winner.length+" gap-2 place-items-center"}>
                                     <div>{createWinner(props.winner)}</div>
                                 </div>
                                 <h3 className="text-9xl leading-6 font-extrabold text-gray-900 text-center " id="modal-title">
-                                    Win!
+                                    {props.winner.length === 2 ? "Draw" : "Win!"}
                                 </h3>
                                 <div className="mt-20">
                                     <p className="text-sm text-gray-500">
